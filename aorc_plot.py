21021390f982da2fa1b6d4b64693138067f244c3
Myles <mmcmanus@thewaterinstitute.org>
2024-07-25 23:02:36 -0500

    init

---
+++ aorc_plot.py
@@ -0,0 +1,61 @@
+'''
+Plot AORC data for multiple events
+Each event will have multiple .nc files
+Aggregate the data for each event and plot the data at a specific location(s).
+Two plots will be created:
+1. Incremental
+2. Cumulative
+'''
+
+import xarray as xr
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob
+
+# Define the location of the data, assumes the data will be in a directory of the form:
+# /data_dir/event/*.nc
+data_dir = r"V:\projects\p00659_dec_glo_phase3\00_collection\Precip"
+events = [
+    'Harvey',
+    'Ike',
+    'Rita',
+    'March 2022'
+]
+
+# Define the coordinates for each location to use for the plots.
+# The coordinates are in the form (lat, lon)
+# using the USGS Gage for GLO (29.6756, -94.6661)
+locations = [
+    (29.6756, -94.6661)
+]
+
+# For each event, read in the data and aggregate the data for each location
+for event in events:
+    # Get the list of files for the event
+    files = glob.glob(f'{data_dir}/{event}/*.nc4')
+    # remove files that dont start with 'AORC_APCP'
+    # files2 = [f for f in files if 'AORC_APCP' in f]
+    # Read in the data
+
+    data = xr.open_mfdataset(files)
+    # Aggregate the data for each location
+    for lat, lon in locations:
+        # Get the data for the location
+        data_loc = data.sel(lat=lat, lon=lon, method='nearest')
+        # Calculate the incremental and cumulative precipitation
+        data_loc['precip_inc'] = data_loc['precipitationCal'].diff(dim='time', label='lower')
+        data_loc['precip_cum'] = data_loc['precipitationCal'].cumsum(dim='time')
+        # Plot the data
+        fig, ax = plt.subplots(2, 1, figsize=(10, 10))
+        # Incremental plot
+        data_loc['precip_inc'].plot(ax=ax[0])
+        ax[0].set_title(f'{event} Incremental Precipitation')
+        ax[0].set_ylabel('Precipitation (mm)')
+        # Cumulative plot
+        data_loc['precip_cum'].plot(ax=ax[1])
+        ax[1].set_title(f'{event} Cumulative Precipitation')
+        ax[1].set_ylabel('Precipitation (mm)')
+        plt.show()
+        # Save the plot
+        plt.savefig(f'{event}_{lat}_{lon}.png')
